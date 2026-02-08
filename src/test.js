a371aec9a2fbb9fdf5694b1b871bcbde1093875f
Andrews Oppong - TOT <andrews.oppongx@gmail.com>
2026-02-08 10:16:59 +0000

    Fix test.js to properly exit with code 0

--- src/test.js
+++ src/test.js
@@ -1,7 +1,8 @@
 console.log('Starting test...');
 
 setTimeout(() => {
-    console.log('waiting 3secons!');
+    console.log('waiting 3 seconds!');
+    process.exit(0);
 }, 3000);
 
 console.log('Finished test!');
